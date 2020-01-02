Corrected Teff calculation for charging

v2g/battery.py
@@ -36,18 +36,16 @@ class Battery:
         c = -0.2847
         d = 0.9475
         m = 1.4
-        n = 2
-        Voc = a + b * (-np.log(soc))**m + c * soc + d * np.exp(n * (soc - 1))
-        if isinstance(Voc, (list, np.ndarray)):
-            if np.isnan(Voc).any() == True :
-                index = np.isnan(Voc)
-                Voc[index] = 4.2
+        n = 2.0
+        Voc = a + b * (-np.log(soc) + 10**(-14))**m + c * soc + d * np.exp(n * (soc - 1))
         return Voc
 
     def charge(self, rate, T, temperature=298.15, stop_soc = 1.0, subinterval_limit = 10):
         ''' Charge battery at rate for time T in hours, optionally stopping once soc reaches stop point.
         '''
         if self.soc >= stop_soc:
+            return 0
+        if self.soc == 1.0:
             return 0
         #just to crash on being given a date or something
         T = float(T)
@@ -71,8 +69,8 @@ class Battery:
         Eac2 = 35000				# J/mol
         alpha_c2 =  0.023
         beta_c2 = 2.61
-        #compute if we will stop due to SOC or time, gives hours of charging
-        Teff = min(T, (stop_soc - self.soc) * self.capacity / rate)
+        #compute if we will stop due to SOC or time, gives hours of charging 
+        Teff = min(T, (stop_soc - self.soc) * self.capacity / rate * np.sum(np.array([(1.0 - self.eff)**i for i in range(math.floor(T))])))
         #compute N - fractional cycle
         N = Teff * rate * self.eff / self.capacity
 
@@ -87,16 +85,13 @@ class Battery:
             * np.exp(alpha_b1 * F / Rug *(voc/temperature - V_ref/T_ref)) \
             * ((1 + (1 - self.soc)/Dod_ref)**beta_b1)
         #trapezoidal
-        self.b1 += b1_ref * np.sum((y[1:] + y[:-1]) * (time_grid[1:] - time_grid[:-1]) * 0.5)
-
-        if not self.b1<1:
-            print(self.b1)
-            print( soc, voc, np.exp(alpha_b1 * F / Rug *(voc/temperature - V_ref/T_ref)))
+        # added 10**(-15) to avoid nans
+        self.b1 += max(0.1**15, b1_ref * np.sum((y[1:] + y[:-1]) * (time_grid[1:] - time_grid[:-1]) * 0.5))
 
         y =  np.exp(-Eac2/Rug * (1/temperature - 1/T_ref)) \
 		* np.exp(alpha_c2 * F / Rug *(voc/temperature - V_ref/T_ref)) \
 		* (N * ((1 - self.soc)/Dod_ref)**beta_c2)
-        self.c2 += c2_ref * np.sum((y[1:] + y[:-1]) * (time_grid[1:] - time_grid[:-1]) * 0.5)
+        self.c2 += max(0.1**15, c2_ref * np.sum((y[1:] + y[:-1]) * (time_grid[1:] - time_grid[:-1]) * 0.5))
 
         self.soc += N
         self.hour_age += T

v2g/iutils.py
@@ -154,7 +154,7 @@ def dist_time_battery_correlated_sampling(dist, time, ev_range, N, DoD = 0.9, SF
 		raise ValueError('Size of ev_range must equal N')
 	length = len(dist)
 	p = np.asarray([1.0/length]*length)
-	x = 1000	#start with a high commute distance
+	x = 1000.0	#start with a high commute distance
 	sampled_dist = np.zeros(N)
 	sampled_time = np.zeros(N)
 	for i in range(N):
@@ -205,8 +205,8 @@ seed = None, charging_rate = 11.5, eff=0.78, SF = 0.3, DoD = 0.9):
 
 			#go to work
 			#driving discharge
-			battery.discharge(battery_used_for_travel/2 / commute_time / 60, commute_time / 60, eff=1.0)
-			commute_battery.discharge(battery_used_for_travel/2 / commute_time / 60, commute_time / 60, eff=1.0)
+			battery.discharge(battery_used_for_travel/2.0 / commute_time / 60.0, commute_time / 60.0, eff=1.0)
+			commute_battery.discharge(battery_used_for_travel/2.0 / commute_time / 60.0, commute_time / 60.0, eff=1.0)
 
 			time_discharge_starts = round_dt_up(time_arrival_work)
 

v2g/v2g_optimize.py
@@ -10,7 +10,7 @@ from v2g.iutils import *
 import fire, tqdm, math, os, scipy.optimize
 
 
-def v2g_optimize(state_list=None, sell_prices=[], prefix='.', samples=2813, SF=0.3, degredation=True, efficiency=0.78, battery_cap_cost = 209, charging_rate = 11.5):
+def v2g_optimize(state_list=None, sell_prices=[], prefix='.', samples=2813, SF=0.3, degredation=True, efficiency=0.78, battery_cap_cost = 209.0, charging_rate = 11.5):
 	#total EVs in NYC Sept2018 default samples
 	if type(state_list) == str:
 		state_list = [state_list]
@@ -170,7 +170,6 @@ def v2g_optimize(state_list=None, sell_prices=[], prefix='.', samples=2813, SF=0
 		results = {'Distance': commute_dist, 'Time':commute_time, 'Work hours': weekly_work_hrs, 'Work time': time_depart_from_home, 'Battery': battery, 'OSP_Savings': max_savings, 'OSP': osp, 'Pricetaker_Savings': pricetaker_savings, 'capacity_fade': capacity_fade, 'Commute_cycles': commute_cycles, 'v2g_cycles': v2g_cycles, 'pt_cycles': pt_cycles, 'osp_charging': charging_cost, 'pt_charging': pt_charging_cost, 'osp_discharging': discharging_cost, 'commute_cost': commute_cost, 'pt_capacity_fade': pt_cap_fade }
 		results = pd.DataFrame.from_dict(results)
 		results.to_csv(data_file)
-
 
 def main():
 	fire.Fire(v2g_optimize)